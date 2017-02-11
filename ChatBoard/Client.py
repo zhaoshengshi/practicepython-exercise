062d04d5e9c6ba44880df2d53b3c71ae7c8a8179
Matthew Qiu <zhaoshengshi@gmail.com>
2017-02-11 17:34:30 +1100

    update Chat Board

--- ChatBoard/Client.py
+++ ChatBoard/Client.py
@@ -1,15 +1,16 @@
 import socket
-import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 8000))
 
 while True:
     try:
-        inp = (input('localhost: ') + '\n').encode()
-        s.send(inp)
-    except KeyboardInterrupt:
-        print('break from user')
+        inp = input('localhost: ') + '\n'
+        s.send(inp.encode())
+    except (KeyboardInterrupt, ConnectionResetError):
+        print('Disconnected...')
         break
+
+
 s.shutdown(socket.SHUT_RDWR)
 s.close()
